cf7ff82aec7e6db2c672e1db17de705f0bffdc4c
takeru ohta <ohta@oktop.(none)>
2010-08-29 23:44:17 +0900

    common-prefix-searchコマンド用のjavadoc記述が終了

--- src/net/reduls/jada/bin/CommonPrefixSearch.java
+++ src/net/reduls/jada/bin/CommonPrefixSearch.java
@@ -12,16 +12,20 @@ import java.io.IOException;
  */
 public final class CommonPrefixSearch {
     /**
-     * 検索対象のキーリストを標準入力から読み込み、対応するIDを検索/出力する。<br />
-     * キーに対応するIDが存在しない場合は、-1が出力される。<br />
+     * 検索対象のキーリストを標準入力から読み込み、共通接頭辞検索を行った結果を標準出力に出力する。<br />
+     * <br />
+     * <h><b>【出力フォーマット】</b></h><br />
+     * 以下の繰り返し:<br />
+     * <div style="background:#DDDDDD">
+     * 検索対象キー[改行]<br />
+     * [タブ文字][マッチした共通接頭文字列][タブ文字][ID値][改行]<br />
+     * </div>
      * <br />
      * <h><b>【コマンドライン引数】</b></h><br />
-     * {@code $ java net.reduls.jada.bin.Search [--bench] index < key-list > id-list}<br />
+     * {@code $ java net.reduls.jada.bin.CommonPrefixSearch index < key-list}<br />
      * <table border="1">
-     * <tr><td><b>--bench:</b></td><td>指定された場合は、IDの出力ではなく検索処理のベンチマークを行う。</td></tr>
      * <tr><td><b>index:</b></td><td>検索に用いる構築済みトライが保存されているファイルのパス。</td></tr>
      * <tr><td><b>key-list:</b></td><td>検索対象となるキーのリスト。標準入力から読み込む。</td></tr>
-     * <tr><td><b>id-list:</b></td><td>入力キーリストに対応するIDのリスト。標準出力に出力される。--benchオプションが指定されている場合は出力なし。</td></tr>
      * </table>
      *
      * @param args コマンドライン引数
@@ -40,9 +44,9 @@ public final class CommonPrefixSearch {
             Trie.CharSequenceCodeStream in = new Trie.CharSequenceCodeStream(line);
             Trie.Node node = new Trie.Node();
             while(srch.commonPrefixSearch(in, node))
-                System.out.println("\t"+line.substring(0, in.offset())+"#"+node.id());
+                System.out.println("\t"+line.substring(0, in.offset())+"\t"+node.id());
             if(node.id()!=-1)
-                System.out.println("\t"+line.substring(0, in.offset())+"#"+node.id());
+                System.out.println("\t"+line.substring(0, in.offset())+"\t"+node.id());
         }
     }
 }
